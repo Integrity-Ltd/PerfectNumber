33cbc7721632bda9aa579d8e06d295c3c00b9e70
Tibor Dravecz <t.gthb+17@integrity.hu>
2017-04-03 09:48:55 +0200

    Update perfectnumber.cc

--- perfectnumber.cc
+++ perfectnumber.cc
@@ -3,15 +3,23 @@
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/miller_rabin.hpp>
 
-#define number(size) boost::multiprecision::number<boost::multiprecision::backends::cpp_int_backend<size, size>>
+constexpr unsigned mersenne_max = 16392;  // 2 ^ x (2 ^ 12)
+constexpr unsigned perfect_max = 2 * mersenne_max;
+constexpr unsigned precision = 25;
+
+namespace boost {
+namespace multiprecision {
+using boost::multiprecision::backends::cpp_int_backend;
+typedef number<cpp_int_backend<mersenne_max, mersenne_max, unsigned_magnitude, unchecked, void> > uint_mersenne_t;
+typedef number<cpp_int_backend<perfect_max, perfect_max, unsigned_magnitude, unchecked, void> > uint_perfect_t;
+}
+}
+using boost::multiprecision::uint_mersenne_t;
+using boost::multiprecision::uint_perfect_t;
 using boost::multiprecision::miller_rabin_test;
 
 #define TIME
 // #define TIME_2
-
-const unsigned mersenne_max = 16392; // 2 ^ x (2 ^ 12)
-const unsigned perfect_max = 2 * mersenne_max;
-const unsigned precision = 25;
 
 #ifdef TIME
 #include <chrono>
@@ -21,47 +29,40 @@ using std::chrono::duration_cast;
 #define TIME_CAST std::chrono::seconds
 #define TIME_CAST_NAME " seconds"
 void printDuration(const steady_clock::time_point &start,
-		const std::string &msg) {
-	steady_clock::time_point end = steady_clock::now();
-	std::cout << msg << duration_cast<TIME_CAST>(end - start).count()
-	<< TIME_CAST_NAME << std::endl;
+                   const std::string &msg) {
+  steady_clock::time_point end = steady_clock::now();
+  std::cout << msg << duration_cast<TIME_CAST>(end - start).count()
+            << TIME_CAST_NAME << std::endl;
 }
-#endif // TIME
+#endif  // TIME
 
-inline number(mersenne_max)power_mersenne_max(uint64_t p) {
-	number(mersenne_max) rtn = 1;
-	for (; p > 0; p--) {
-		rtn *= 2;
-	}
-	return rtn;
-}
-
-inline number(perfect_max)power_perfect_max(uint64_t p) {
-	number(perfect_max) rtn = 1;
-	for (; p > 0; p--) {
-		rtn *= 2;
-	}
-	return rtn;
+template <typename Tp_>
+inline Tp_ power(uint64_t p) {
+  Tp_ rtn = 1;
+  for (; p > 0; p--) {
+    rtn *= 2;
+  }
+  return rtn;
 }
 
 int main() {
 #ifdef TIME
-	steady_clock::time_point start = steady_clock::now();
-#endif // TIME
-	for (uint64_t p = 1; p < mersenne_max; p++) {
-		if (miller_rabin_test(p, precision)) {
-			number(mersenne_max)mersenne = power_mersenne_max(p) - 1;
-			if (miller_rabin_test(mersenne, precision)) {
-				number(perfect_max) perfent_number = (number(perfect_max)) mersenne * power_perfect_max(p - 1);
-				std::cout << p << ":	" << perfent_number << std::endl;
+  steady_clock::time_point start = steady_clock::now();
+#endif  // TIME
+  for (uint64_t p = 1; p < mersenne_max; p++) {
+    if (miller_rabin_test(p, precision)) {
+      uint_mersenne_t mersenne = power<uint_mersenne_t>(p) - 1;
+      if (miller_rabin_test(mersenne, precision)) {
+        uint_perfect_t perfent_number = static_cast<uint_perfect_t>(mersenne) * power<uint_perfect_t>(p - 1);
+        std::cout << p << ":	" << perfent_number << std::endl;
 #ifdef TIME_2
-				printDuration(start, "Calculating took: ");
-#endif // TIME_2
-			}
-		}
-	}
+        printDuration(start, "Calculating took: ");
+#endif  // TIME_2
+      }
+    }
+  }
 #ifdef TIME
-	printDuration(start, "Calculating took: ");
-#endif // TIME
-	return 0;
+  printDuration(start, "Calculating took: ");
+#endif  // TIME
+  return 0;
 }
